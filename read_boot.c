04c2d78fbbc5eae9a037c42ccb23f25063ed9d55
Diego Lopez <diegolopezpaez3464@gmail.com>
2024-03-31 01:42:50 -0300

    Agregados los atributos faltantes al struct de Fat12BootSector y completados los prints correspondientes

--- read_boot.c
+++ read_boot.c
@@ -15,6 +15,21 @@ typedef struct {
     char oem[8];
     unsigned short sector_size; // 2 bytes
 	// {...}  COMPLETAR
+    unsigned char sectors_per_cluster;
+    unsigned short reserved_area_size;
+    unsigned char num_of_fat;
+    unsigned short max_num_of_files_in_root;
+    unsigned short num_of_sectors_in_fs;
+    char media_type;
+    unsigned short fat_size;
+    unsigned short sectors_per_track;
+    unsigned short heads;
+    unsigned int num_of_sectors_before_start_partition;
+    unsigned int fs_sectors;
+    char drive_number;
+    char not_used; //xd
+    char extended_boot_signature;
+    //
     unsigned int volume_id;
     char volume_label[11];
     char fs_type[8]; // Type en ascii
@@ -28,7 +43,9 @@ int main() {
     PartitionTable pt[4];
     Fat12BootSector bs;
     
-    fseek(in, ... , SEEK_SET); // Ir al inicio de la tabla de particiones. Completar ...
+    
+    //lo hardcodee, ver forma de obtener el 446
+    fseek(in, 446 , SEEK_SET); // Ir al inicio de la tabla de particiones. Completar ...
     fread(pt, sizeof(PartitionTable), 4, in); // leo entradas 
     
     for(i=0; i<4; i++) {        
@@ -43,7 +60,7 @@ int main() {
         printf("No se encontró filesystem FAT12, saliendo ...\n");
         return -1;
     }
-    
+
     fseek(in, 0, SEEK_SET);
     fread(&bs, sizeof(Fat12BootSector), 1, in);
     
@@ -51,6 +68,21 @@ int main() {
     printf("  OEM code: [%.8s]\n", bs.oem);
     printf("  sector_size: %d\n", bs.sector_size);
 	// {...} COMPLETAR
+    printf("  Sectors per cluster: %d\n", bs.sectors_per_cluster);
+    printf("  Reserved area size, in sectors: %d\n", bs.reserved_area_size);
+    printf("  Number of FATs: %d\n", bs.num_of_fat);
+    printf("  Max number of files in the root directory: %d\n", bs.max_num_of_files_in_root);
+    printf("  Number of sectors in the file system: %d\n", bs.num_of_sectors_in_fs);
+    printf("  Media type: %02X\n", bs.media_type);
+    printf("  Size of each FAT, in sectors: %d\n", bs.fat_size);
+    printf("  Number per track in storage device: %d\n", bs.sectors_per_track);
+    printf("  Number of heads in storage device: %d\n", bs.heads);
+    printf("  Number of sectors before the start partition: %d\n", bs.num_of_sectors_before_start_partition);
+    printf("  Number of sectors in the file system: %d\n", bs.fs_sectors);
+    printf("  BIOS INT 13h drivce number: %02X\n", bs.drive_number);
+    printf("  Byte not used: %02X\n", bs.not_used);
+    printf("  Extended boot signature: %02X\n", bs.extended_boot_signature);
+    //
     printf("  volume_id: 0x%08X\n", bs.volume_id);
     printf("  Volume label: [%.11s]\n", bs.volume_label);
     printf("  Filesystem type: [%.8s]\n", bs.fs_type);
